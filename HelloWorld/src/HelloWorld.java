0997e4cda01f72d28694a41aec18e88f725cccfa
dilz <dilz.in@gmail.com>
2016-12-28 15:41:11 -0800

    adding new projects

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello world! Happy new year!");
+		int x = 10;
+		while (x > 4) {
+		 System.out.print(" Hello"+x );
+		 System.out.print("");
+		 x = x - 1;
+	        
+		}
+		
+	}
+
+}
---
+++ Practice/src/PracticeGuess.java
@@ -0,0 +1,17 @@
+
+public class PracticeGuess {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean guess1 = false;
+		boolean guess2 = true;
+		boolean guess3 = false;
+		
+		System.out.print("guess1 is "+ guess1 + "\n");
+		System.out.print("guess1 is "+ guess2 + "\n");
+		System.out.print("guess1 is "+ guess3 + "\n");
+		
+
+	}
+
+}
